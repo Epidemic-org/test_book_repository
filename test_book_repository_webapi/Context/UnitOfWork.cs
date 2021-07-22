bc5ad9efc62e854ebd157b981993b78d465804eb
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-22 19:44:39 +0430

    add-unit-of-work

---
+++ test_book_repository_webapi/Context/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using test_book_repository_webapi.Contracts;
+using test_book_repository_webapi.Models;
+using test_book_repository_webapi.Repositories;
+
+namespace test_book_repository_webapi.Context
+{
+    public class UnitOfWork
+    {
+        private IGenericRepository<Book> _gnrBookRepository;
+        public IGenericRepository<Book> GenericRepository {
+            get {
+                if (_gnrBookRepository == null) {
+                    _gnrBookRepository = new GenericRepository<Book>();
+                }
+                return _gnrBookRepository;
+            }
+        }
+    }
+}
+
+
--- test_book_repository_webapi/Repositories/BookRepository.cs
+++ test_book_repository_webapi/Repositories/BookRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using test_book_repository_webapi.Contracts;
+using test_book_repository_webapi.Models;
 
 namespace test_book_repository_webapi.Repositories
 {
-    public class BookRepository:IBookRepository
+    public class BookRepository
     {
 
     }
